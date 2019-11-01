f6fa67c905ba2a6280cd3204a10540e162f0f37b
zhangyanlong <1944293561@qq.com>
2019-11-01 17:53:51 +0800

    整合AOP

---
+++ src/main/java/com/example/springboot/Test.java
@@ -0,0 +1,22 @@
+package com.example.springboot;
+
+/**
+ * @author zhangYanLong
+ * @date 2019/10/30
+ */
+class Solution {
+
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(i!=j) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i,j};
+                    }
+                }
+            }
+        }
+        throw  new IllegalArgumentException("No two");
+    }
+}
